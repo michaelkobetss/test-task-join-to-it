cb52dede3dc6ab0003a57ea84060459c232343a2
Michael Kobets <michael.kobetss@gmail.com>
2025-10-18 20:12:05 +0300

    2.0.1
    fixed logo

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,4 @@
-// CalendarView.jsx
-"use client"
+
 import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
